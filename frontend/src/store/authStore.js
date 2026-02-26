ee971382ab45d431e18112a1b5409071d1eef2ce
heet <heetopt@gmail.com>
2026-02-26 22:03:11 +0530

    feat(frontend): initial setup done

--- frontend/src/store/authStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-export const useAuthStore = create(set => ({
-  user: null,
-  isAuthenticated: false,
-
-  setUser: userData => set({ user: userData, isAuthenticated: true }),
-  logout: () => set({ user: null, isAuthenticated: false })
-}));
